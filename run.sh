3777f1c1465363769b8c3c3bf44ba2ec7f657845
Chun-Min Chang <chun.m.chang@gmail.com>
2021-06-24 10:19:18 -0700

    Show error messages in run script

--- run.sh
+++ run.sh
@@ -1,16 +1,27 @@
+run_dir() {
+    local folder=$1
+    cd $folder
+    make
+
+    for file in $(ls); do
+        if [ -x "$file" ] && [ "$file" != "$self" ]; then
+            echo "\nRun $file:"
+            ./$file
+            echo "\n"
+        fi
+    done
+
+    make clean
+    cd ..
+}
+
 self=$(basename $0)
-folder=$1
 
-cd $folder
-make
-
-for file in $(ls);do
-    if [ -x "$file" ] && [ "$file" != "$self" ]; then
-        echo "\nRun $file:"
-        ./$file
-        echo "\n"
-    fi
-done
-
-make clean
-cd ..
+if [ -z "$1" ] ; then
+    echo "Please set a directory"
+elif [ ! -d "$1" ]; then
+    echo "$1 is not a directory"
+else
+    echo "Run examples in $1:\n==========\n"
+    run_dir $1
+fi
